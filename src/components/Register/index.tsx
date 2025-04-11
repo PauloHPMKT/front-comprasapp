3d71b82cd9de0f21cc528c00bd96e2b7ac3edf9c
Paulo Sergio <pauloserg861@gmail.com>
2025-04-11 13:09:40 -0300

    feat: add register view

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,8 +1,12 @@
 import { BaseInput } from "../BaseInput";
 
-export function Login() {
+export function Register() {
   return (
     <form>
+      <p className="text-[14px] mb-6 font-semibold text-center px-8">Insira aqui alguns dados para criar sua lista de compras</p>
+      <BaseInput
+        placeholder="Como você quer ser chamado?"
+      />
       <BaseInput
         placeholder="Insira seu e-mail"
       />
@@ -10,13 +14,10 @@ export function Login() {
         placeholder="Senha"
         type="password"
       />
-      <div className="flex mb-11 items-start justify-between">
-        <label htmlFor="remember" className="flex items-center gap-2">
-          <input type="checkbox" id="remember" />
-          Lembrar-me
-        </label>
-        <p>Esqueci minha senha</p>
-      </div>
+      <BaseInput
+        placeholder="Confirme sua senha"
+        type="password"
+      />
       <div className="flex flex-col gap-4 justify-center items-center">
         <button
           className="bg-red-600 text-white w-full h-11 p-2 rounded-3xl"
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,10 +1,14 @@
-import MainLogoComprasApp from "../assets/main-logo.svg"
+import { useState } from "react";
+import MainLogoComprasApp from "../assets/main-logo.svg";
 import { Login } from "../components/Login";
+import { Register } from "../components/Register";
 
 export function Account() {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
     <div className="h-screen w-full m-auto flex items-center justify-center">
-      <div className="flex flex-col w-96 p-8 bg-white">
+      <div className="flex flex-col w-96 p-8 mb-30">
         <div className="flex items-center justify-center">
           <img
             src={MainLogoComprasApp}
@@ -12,7 +16,18 @@ export function Account() {
             className="w-[250px] mb-8"
           />
         </div>
-        <Login />
+        {isLogin
+          ? <Login />
+          : <Register />
+        }
+        <div className="mt-8 flex flex-col gap-4 justify-center items-center">
+          <button
+            onClick={() => setIsLogin(!isLogin)}
+            className="text-red-500 underline"
+          >
+            {isLogin ? "Não tem uma conta? Cadastre-se" : "Já tem uma conta? Faça login"}
+          </button>
+        </div>
       </div>
     </div>
   );
