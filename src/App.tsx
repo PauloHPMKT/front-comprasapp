a5cb0d058365deae1081834048e00ba5ae7e65e3
Paulo Sergio <pauloserg861@gmail.com>
2025-04-15 16:17:56 -0300

    feat: add template default

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Outlet } from "react-router-dom";
+import inLineLogo from "./assets/inline-logo.svg";
+
 export function App() {
   return (
-    <>
-      <h1 className="bg-red-500">Teste</h1>
-    </>
+    <div className="py-3 px-5">
+      <header className="flex justify-between">
+        <div className="w-[180px]">
+          <img src={inLineLogo} alt="Logo compras app" className="w-full" />
+        </div>
+        <span className="text-2xl bg-gray-700 rounded-full w-[40px] h-[40px] flex items-center justify-center text-white">
+          H
+        </span>
+      </header>
+      <div className="shadow-[-2px_-2px_8px_rgba(0,0,0,0.1)] bg-white rounded-lg mt-5 p-5 h-[80vh]">
+        <Outlet />
+      </div>
+    </div>
   )
 }
