9cc7f5c989f1e48cf758db0a305ff1021721cce0
Paulo Sergio <pauloserg861@gmail.com>
2025-04-15 16:14:37 -0300

    chore: add hosts config

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-namespace': 'off',
+      'react-hooks/rules-of-hooks': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
