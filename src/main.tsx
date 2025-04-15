14ee97b4300a833352fbe175f18d83bd970a099b
Paulo Sergio <pauloserg861@gmail.com>
2025-04-15 16:17:34 -0300

    feat: implement toastify to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import './index.css'
@@ -8,6 +9,7 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
+    <ToastContainer />
     <RouterProvider router={router} />
   </QueryClientProvider>
 )
