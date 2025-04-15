4b976fe112e6547a3eeace09950faf1cdbf0a064
Paulo Sergio <pauloserg861@gmail.com>
2025-04-15 16:17:13 -0300

    feat: add app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,30 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Account } from "../pages/Account";
+import { Home } from "../pages/Home";
+import { App } from "../App";
+import { CreateList } from "../pages/CreateList";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <Navigate to="/account" replace />,
+  },
+  {
+    path: "/account",
     element: <Account />,
+  },
+  {
+    path: "/app",
+    element: <App />,
+    children: [
+      {
+        path: "list",
+        element: <Home />,
+      },
+      {
+        path: "list/add",
+        element: <CreateList />,
+      }
+    ]
   }
 ])
